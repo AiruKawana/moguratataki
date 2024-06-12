4cf8a329925e4887b9bea2fefc4b515839afca2f
kawanaairu <airu92256@gmail.com>
2024-06-12 09:45:23 +0900

    初めてのコミット

---
+++ Assets/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI lasttimeText;
+    [SerializeField] private float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 60.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(0 <= time)
+        {
+            time -= Time.deltaTime;
+            lasttimeText.text = "Last Time:" + time.ToString("F1");
+        } 
+        else if(0 >= time)
+        {
+            lasttimeText.text = "TimeUp";
+        }
+    }
+}
---
+++ Assets/Script/MoleAction.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MoleAction : MonoBehaviour
+{
+ 
+    [SerializeField] private Vector3 molestartPosition;
+    [SerializeField] private float targetPosition = -4.5f;
+    [SerializeField] private float startYPosition = -7.71f;
+    [SerializeField] private float upRange = 0.02f;
+    [SerializeField] private float downRange = -0.02f;
+    [SerializeField] private float secondsTimer = 0.01f;
+
+    //[SerializeField] private GameObject mole1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("MoleMoveUp");
+    }
+
+   IEnumerator MoleMoveUp()
+    {
+        while (molestartPosition.y < targetPosition)
+        {
+            molestartPosition = transform.position;
+            transform.Translate(0, upRange, 0);
+            yield return new WaitForSeconds(secondsTimer);
+            StartCoroutine("MoleMoveDown");
+        }
+    }
+
+    IEnumerator MoleMoveDown()
+    {
+        if(molestartPosition.y > targetPosition)
+        {
+            while(molestartPosition.y > startYPosition)
+            {
+                molestartPosition = transform.position;
+                transform.Translate(0, downRange, 0);
+                yield return new WaitForSeconds(secondsTimer);
+            }   
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
